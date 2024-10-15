fafb24dfad33bb0a8f8f15ab38aa69b7dce3096d
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2024-10-15 12:58:13 -0500

    fix: button pointer

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,8 +2,9 @@ export default function Button({ text, link }) {
   return (
     <>
       <a
-        href={link} target="blank_"
-        className="group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-black/20 w-52 h-16 font-Syne font-bold tracking-tighter text-white dark:bg-white/20"
+        href={link}
+        target="blank_"
+        className="group relative inline-flex h-16 w-52 cursor-pointer items-center justify-center overflow-hidden rounded-full bg-black/20 font-Syne font-bold tracking-tighter text-white dark:bg-white/20"
       >
         <span className="absolute h-0 w-0 rounded-full bg-white/50 transition-all duration-500 ease-out group-hover:h-56 group-hover:w-56"></span>
 
